84e78a3fc21d8a3dbfa75a51d9775bb6f88fed76
Luiz Brito da Rosa <luizbrt989@gmail.com>
2020-12-11 18:19:27 -0300

    X

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+enum Type {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +25,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private sumBalanceByTypes(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((sum, tr) => sum + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumBalanceByTypes(Type.INCOME);
+    const outcome = this.sumBalanceByTypes(Type.OUTCOME);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    const { total } = this.getBalance();
+    if (type === Type.OUTCOME && total < value) {
+      throw Error('Can not create outcome transaction without a valid balance');
+    }
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
